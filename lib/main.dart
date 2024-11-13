2f20ecee3653da45c1ac32f992a946325a29b41d
adrianorinaldi <adriano-rinaldi@hotmail.com>
2024-11-13 07:41:38 -0300

    0.5

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todos/stores/app.store.dart';
 import 'package:todos/themes/app.theme.dart';
 import 'package:todos/views/create-todo.view.dart';
 import 'package:todos/views/home.view.dart';
@@ -12,11 +14,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todos',
-      debugShowCheckedModeBanner: false,
-      theme: appTheme(),
-      home: LoginView(),
+    return MultiProvider(
+      providers: [
+        Provider<AppStore>.value(
+          value: AppStore(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Todos',
+        debugShowCheckedModeBanner: false,
+        theme: appTheme(),
+        home: LoginView(),
+      ),
     );
   }
 }
